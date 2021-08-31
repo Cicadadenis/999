1e7b43fbaae1841d1036f03183b48a7e239b027b
Your Name <you@example.com>
2021-08-31 23:28:02 +0300

    Initial commit

---
+++ menu.py
@@ -0,0 +1,354 @@
+from os import system
+import os, sys, time
+import os, sys
+import time
+import traceback
+import time
+import webbrowser
+
+
+
+
+
+
+def clearscreen():
+	if sys.platform == "linux2":
+		os.system("cls")
+	elif sys.platform == "win32":
+		os.system("cls")
+	else:
+		os.system("cls")
+        
+def restart_program():
+	python = sys.executable
+	os.execl(python, python, * sys.argv)
+	curdir = os.getcwd()
+    
+    
+def backtomenu_option():
+	backtomenu = input("    Ваш Выбор > ")
+
+	if backtomenu == "99":
+		restart_program()
+	elif backtomenu == "00":
+		sys.exit()
+	else:
+		print("\nОШИБКА: неверный ввод")
+		time.sleep(1)
+		restart_program()
+ 
+re = ("\033[1;31m")
+gr = "\033[1;31m"
+cy = ("\033[1;36m")
+ 
+
+
+os.system('cls')
+re = "\033[1;31m"
+gr = "\033[1;32m"
+cy="\033[1;36m"
+
+logo = (f"""
+          _             __         {re}___       __{cy}
+     ____(_)______ ____/ /__ _____{re}/ _ )___  / /_{cy}
+    / __/ / __/ _ `/ _  / _ `/___{re}/ _  / _ \/ __/{cy}
+    \__/_/\__/\_,_/\_,_/\_,_/   {re}/____/\___/\__/{cy}
+
+    ----------Telegram-Bot-PhotoHosting-----------
+
+	Для работы этой программы нужны три вспомогательных 
+	
+		1. Python3.9 (при установке установить галочку на Path)
+		
+		2. Git(github) (все по дефолту)
+		
+		3. Heroku 
+        
+        их можно скачать дальше 
+
+    """)
+    
+print(logo)
+input('Enter чтобы начать')
+print(f"""
+      
+    {re} _{cy}
+   {re} / {cy}
+   {re} \_{cy}
+      
+      """)
+   
+os.system('cls')
+time.sleep(0.01)
+
+print(f"""
+       
+    {re} __{cy}
+   {re} / _{cy}
+   {re} \__{cy}
+
+        """)
+
+    
+os.system('cls')
+time.sleep(0.01)
+print(f"""
+        
+     {re}___{cy}
+    {re}/ __{cy}
+  {re}  \__/{cy}
+
+        """)
+        
+      
+os.system('cls')
+time.sleep(0.01)
+print(f"""
+     
+     {re}____({cy}
+   {re} / __/ {cy}
+    {re}\__/_/{cy}
+
+            """)
+      
+os.system('cls')
+time.sleep(0.01)
+print(f"""
+         {re} _ {cy}
+    {re} ____(_){cy}
+    {re}/ __/ / {cy}
+    {re}\__/_/\_{cy}
+            
+  """)
+
+
+      
+os.system('cls')
+time.sleep(0.01)
+
+print(f"""
+        {re}  _ {cy}   
+    {re} ____(_)___{cy}
+   {re} / __/ / __/{cy}
+   {re} \__/_/\__/\{cy}
+
+                """)
+
+
+   
+os.system('cls')
+time.sleep(0.01)
+
+print(f"""
+        {re}  _       {cy}
+    {re} ____(_)______{cy}
+   {re} / __/ / __/ _ {cy}
+   {re} \__/_/\__/\_,_{cy}
+                  
+                  """)
+
+
+     
+os.system('cls')
+time.sleep(0.01)
+
+print(f"""
+         {re} _          {cy}
+     {re}____(_)______ __{cy}
+    {re}/ __/ / __/ _ `/ {cy}
+    {re}\__/_/\__/\_,_/\_{cy}
+                     
+                        """)
+
+
+     
+os.system('cls')
+time.sleep(0.01)
+
+print(f"""
+         {re} _             _{re}
+     {re}____(_)______ ____/ {re}
+    {re}/ __/ / __/ _ `/ _  /{re}
+    {re}\__/_/\__/\_,_/\_,_/\{re}
+                         
+                         """)
+
+
+ 
+os.system('cls')
+time.sleep(0.01) 
+
+print(f"""
+         {re} _             __   {cy}
+     {re}____(_)______ ____/ /__ {cy}
+    {re}/ __/ / __/ _ `/ _  / _ `{cy}
+    {re}\__/_/\__/\_,_/\_,_/\_,_/{cy}
+             
+""")
+
+
+    
+os.system('cls')
+time.sleep(0.01)
+
+print(f"""
+         {re} _             __ {cy}     
+     {re}____(_)______ ____/ /__ __{cy}_
+    {re}/ __/ / __/ _ `/ _  / _ `/ _{cy}
+    {re}\__/_/\__/\_,_/\_,_/\_,_/\__{cy}
+                                    {cy}
+                                """)
+
+
+   
+os.system('cls')
+time.sleep(0.01)
+
+
+print(f"""
+         {re} _             __          {cy}
+     {re}____(_)______ ____/ /__ _______{cy}
+    {re}/ __/ / __/ _ `/ _  / _ `/ __/ _{cy}
+    {re}\__/_/\__/\_,_/\_,_/\_,_/\__/_/ {cy}
+                                        {cy}
+                                    """)
+
+
+
+    
+os.system('cls')
+time.sleep(0.01)
+
+
+
+print(f"""
+        {re}      _             __              {cy}
+         {re}____(_)______ ____/ /__ ___________{cy}
+        {re}/ __/ / __/ _ `/ _  / _ `/ __/ __/ /{cy}
+        {re}\__/_/\__/\_,_/\_,_/\_,_/\__/_/  \_,{cy}
+        {re}                                /___{cy}
+        {re}                            
+                                    """)
+   
+
+
+
+os.system('cls')
+time.sleep(0.01)
+
+
+
+print(f"""
+         {re} _             __                  {cy}
+     {re}____(_)______ ____/ /__ ___________ ___{cy}
+    {re}/ __/ / __/ _ `/ _  / _ `/ __/ __/ // / {cy}
+    {re}\__/_/\__/\_,_/\_,_/\_,_/\__/_/  \_, / .{cy}
+                          {re}          /___/_/ {cy}
+                                    """)
+
+
+      
+os.system('cls')
+time.sleep(0.01)
+
+print(f"""
+         {re} _             __                     {cy}
+     {re}____(_)______ ____/ /__ ___________ _____ {cy}
+    {re}/ __/ / __/ _ `/ _  / _ `/ __/ __/ // / _ \/{cy}
+    {re}\__/_/\__/\_,_/\_,_/\_,_/\__/_/  \_, / .__/{cy}
+ {re}                                   /___/_/    {cy}
+                                    """)
+
+
+   
+os.system('cls')
+time.sleep(0.01)
+
+print(f"""
+         {re} _             __                       __{cy} 
+     {re}____(_)______ ____/ /__ ___________ _____  / /_{cy}
+    {re}/ __/ / __/ _ `/ _  / _ `/ __/ __/ // / _ \/ __/{cy}
+    {re}\__/_/\__/\_,_/\_,_/\_,_/\__/_/  \_, / .__/\__/{cy}
+               {re}                     /___/_/         {cy}
+                                                        """)
+
+
+
+     
+os.system('cls')
+time.sleep(0.01)
+
+print(f"""
+         {re} _             __                       __   {cy}
+     {re}____(_)______ ____/ /__ ___________ _____  / /___{cy}
+    {re}/ __/ / __/ _ `/ _  / _ `/ __/ __/ // / _ \/ __/ _{cy}
+    {re}\__/_/\__/\_,_/\_,_/\_,_/\__/_/  \_, / .__/\__/\__{cy}
+     {re}                               /___/_/           {cy}
+                                    
+                                                        """)
+
+
+
+     
+os.system('cls')
+time.sleep(0.01)
+
+
+print(f"""
+         {re} _             __                       __       /*******/ {cy}
+     {re}____(_)______ ____/ /__ ___________ _____  / /____  /*******/{cy}
+    {re}/ __/ / __/ _ `/ _  / _ `/ __/ __/ // / _ \/ __/ _ \/{cy}
+    {re}\__/_/\__/\_,_/\_,_/\_,_/\__/_/  \_, / .__/\__/\___/{cy}
+         {re}                           /___/_/            {cy}
+                                                        """)
+
+time.sleep(0.01)
+
+menu = (f"""
+
+         {re} _             __                       __       /*******/ {cy}
+     {re}____(_)______ ____/ /__ ___________ _____  / /____  /*******/{cy}
+    {re}/ __/ / __/ _ `/ _  / _ `/ __/ __/ // / _ \/ __/ _ \/{cy}
+    {re}\__/_/\__/\_,_/\_,_/\_,_/\__/_/  \_, / .__/\__/\___/{cy}
+         {re}                           /___/_/            {cy}
+                                                        
+
+  {re} Выберите из меню: {cy}
+  
+	01) Скачать Python3.9
+	02) Скачать Git
+	03) Скачать Heroku 
+	
+        Если все программы установлены для 
+        продолжения напиши start и жми Enter
+        
+        xx) Выход 
+    """)
+ 
+ 
+clearscreen()
+ 
+print(menu)       
+while True:
+    try:
+        santet = input(f" Ваш Выбор ⏩⏩   ")
+
+        # Установка Зависимостей
+        if santet == "01" or santet == "1":
+            webbrowser.open('https://www.python.org/ftp/python/3.9.6/python-3.9.6-amd64.exe')
+        elif santet == "02" or santet == "2":
+            webbrowser.open('https://git-scm.com/download/win')
+        elif santet == "03" or santet == "3":
+            webbrowser.open('https://cli-assets.heroku.com/heroku-x64.exe')
+        elif santet == "start":
+            os.system('pip install pyyaml')
+            os.system('pip install requests')
+            os.system('python Setyp.py')
+        elif santet == "xx" or santet == "x":
+            os.system(exit)
+
+    except ImportError:
+        print("M")
+    except Exception as e:
+        os.system(exit)
+print("Chaoo!!!")
